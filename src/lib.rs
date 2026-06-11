548be0ac7ac1836f97aef7d6c6015ad5387e62bd
schemehcs <94542969+schemehcs@users.noreply.github.com>
2026-06-11 16:50:22 +0800

    expose HMAC_LEN and Hmac type

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
-use sha256::Sha256;
+use sha256::{HASH_LEN, Sha256};
 
-const BS: usize = 64;
-static OPAD: [u8; BS] = [0x5c; BS];
-static IPAD: [u8; BS] = [0x36; BS];
+pub const HMAC_LEN: usize = HASH_LEN;
+pub type Hmac = sha256::Hash;
 
-pub fn hmac_sha256(key: &[u8], message: &[u8]) -> [u8; 32] {
+const BS: usize = HMAC_LEN * 2;
+type Block = [u8; BS];
+
+static OPAD: Block = [0x5c; BS];
+static IPAD: Block = [0x36; BS];
+
+pub fn hmac_sha256(key: &[u8], message: &[u8]) -> Hmac {
     let computed_key = compute_key(key);
     let inner_key = xor_arr(&computed_key, &IPAD);
     let inner_hash = Sha256::new().write(&inner_key).write(message).finish();
@@ -12,22 +17,23 @@ pub fn hmac_sha256(key: &[u8], message: &[u8]) -> [u8; 32] {
     Sha256::new().write(&outer_key).write(&inner_hash).finish()
 }
 
-fn xor_arr(a: &[u8; BS], b: &[u8; BS]) -> [u8; BS] {
-    let mut res: [u8; BS] = *a;
-    for i in 0..BS {
-        res[i] ^= b[i];
-    }
-    res
+fn xor_arr(a: &Block, b: &Block) -> Block {
+    a.iter()
+        .zip(b.iter())
+        .map(|(x, y)| x ^ y)
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
 }
 
-fn compute_key(key: &[u8]) -> [u8; BS] {
-    let mut ckey: [u8; BS] = [0; BS];
+fn compute_key(key: &[u8]) -> Block {
+    let mut padded_key: Block = [0; BS];
     if key.len() < BS {
-        ckey[..key.len()].copy_from_slice(key);
+        padded_key[..key.len()].copy_from_slice(key);
     } else {
-        ckey[..32].copy_from_slice(&sha256::sha256(key));
+        padded_key[..HASH_LEN].copy_from_slice(&sha256::digest(key));
     }
-    ckey
+    padded_key
 }
 
 #[cfg(test)]
