368825d0b37348324ef524b351acce2c54b9257e
schemehcs <94542969+schemehcs@users.noreply.github.com>
2026-06-11 18:18:46 +0800

    refactor with hasher-style

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+use hasher_style::HasherStyle;
 use sha256::{BLOCK_LEN, HASH_LEN, Sha256};
 
 pub const HMAC_LEN: usize = HASH_LEN;
-pub type Hmac = sha256::Hash;
+type Mac = [u8; HMAC_LEN];
 
 const BL: usize = BLOCK_LEN;
 type Block = [u8; BL];
@@ -9,12 +10,48 @@ type Block = [u8; BL];
 static OPAD: Block = [0x5c; BL];
 static IPAD: Block = [0x36; BL];
 
-pub fn hmac_sha256(key: &[u8], message: &[u8]) -> Hmac {
-    let computed_key = compute_key(key);
-    let inner_key = xor_arr(&computed_key, &IPAD);
-    let inner_hash = Sha256::new().write(&inner_key).write(message).finish();
-    let outer_key = xor_arr(&computed_key, &OPAD);
-    Sha256::new().write(&outer_key).write(&inner_hash).finish()
+pub struct Hmac {
+    hasher: Sha256,
+    key: Block,
+}
+
+impl Hmac {
+    pub fn new(key: &[u8]) -> Self {
+        let key = Self::compute_key(key);
+        let ikey = xor_arr(&key, &IPAD);
+        let mut hasher = Sha256::new();
+        hasher.write(&ikey);
+        Hmac { hasher, key }
+    }
+
+    fn compute_key(key: &[u8]) -> Block {
+        let mut padded_key: Block = [0; BL];
+        if key.len() <= BL {
+            padded_key[..key.len()].copy_from_slice(key);
+        } else {
+            padded_key[..HASH_LEN].copy_from_slice(&sha256::digest(key));
+        }
+        padded_key
+    }
+}
+
+impl HasherStyle for Hmac {
+    type Output = Mac;
+    fn write(&mut self, slice: &[u8]) -> &mut Self {
+        self.hasher.write(slice);
+        self
+    }
+
+    fn finish(&mut self) -> Self::Output {
+        let inner_hash = self.hasher.finish();
+        let okey = xor_arr(&self.key, &OPAD);
+        Sha256::new().write(&okey).write(&inner_hash).finish()
+    }
+}
+
+#[inline]
+pub fn hmac(key: &[u8], message: &[u8]) -> Mac {
+    Hmac::new(key).write(message).finish()
 }
 
 fn xor_arr(a: &Block, b: &Block) -> Block {
@@ -26,16 +63,6 @@ fn xor_arr(a: &Block, b: &Block) -> Block {
         .unwrap()
 }
 
-fn compute_key(key: &[u8]) -> Block {
-    let mut padded_key: Block = [0; BL];
-    if key.len() <= BL {
-        padded_key[..key.len()].copy_from_slice(key);
-    } else {
-        padded_key[..HASH_LEN].copy_from_slice(&sha256::digest(key));
-    }
-    padded_key
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,7 +71,7 @@ mod tests {
     fn empty_works() {
         let message = "";
         let key = "";
-        let hmac = hex::encode(&hmac_sha256(key.as_bytes(), message.as_bytes()));
+        let hmac = hex::encode(&hmac(key.as_bytes(), message.as_bytes()));
         let expected = "b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad";
         assert_eq!(expected, hmac);
     }
@@ -54,7 +81,7 @@ mod tests {
         let message = "Hello, world!";
         let key = "b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad\
                     b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad";
-        let hmac = hex::encode(&hmac_sha256(key.as_bytes(), message.as_bytes()));
+        let hmac = hex::encode(&hmac(key.as_bytes(), message.as_bytes()));
         let expected = "70d67669a0e6e3516d37feb9af403d4b36e6aed4efad091ef50c7133b2f9df95";
         assert_eq!(expected, hmac);
     }
